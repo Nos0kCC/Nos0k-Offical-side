cccf304db07cd48902a280652502628ed8049769
Nos0kCC <zakzakk1092@gmail.com>
2025-08-09 16:30:16 +1000

    add logics js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const button = document.getElementById('ghb');
+    
+    button.addEventListener('click', function() {
+        window.open('https://github.com/Nos0kCC', '_blank')
+    })
+});
