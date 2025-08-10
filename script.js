7f470754cdac6c27ff42d19982820ccb661100af
Nos0kCC <zakzakk1092@gmail.com>
2025-08-10 11:13:54 +1000

    script for new html, with logic of new buttons

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const button = document.getElementById('ghb');
+    const ghbutton = document.getElementById('ghb');
+    const tgbutton = document.getElementById('tgb');
+    const STGbutton = document.getElementById('stgb');
+    const SDbutton = document.getElementById('sdb');
+    const SWObutton = document.getElementById('ffb');
+    const SWopen = document.getElementById('pw');
     
-    button.addEventListener('click', function() {
+    ghbutton.addEventListener('click', function() {
         window.open('https://github.com/Nos0kCC', '_blank')
+    })
+    
+    tgbutton.addEventListener('click', function() {
+        window.open('https://t.me/BioNos0k', '_blank')
+    })
+    
+    STGbutton.addEventListener('click', function() {
+        window.open('https://t.me/sun_community_chat', '_blank')
+    })
+    
+    SDbutton.addEventListener('click', function() {
+        window.open('https://discord.gg/BW75DWjZ', '_blank')
+    })
+    
+    SWObutton>addEventListener('click', function() {
+        SWopen.classList.toggle('show');
+    
+    if (SWopen.classList.contains('show')) {
+        SWObutton.textContent = 'скрыть';
+    } else {
+        SWObutton.textContent = 'фанфики с WaterBucket и Error';
+    }
     })
 });
